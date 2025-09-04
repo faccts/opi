ca6305f9ee319a751af89818f560e298f3819c23
haneug <38649381+haneug@users.noreply.github.com>
2025-09-04 08:14:15 +0200

    Add further convenience functions, run nox
    - Add convenience functions `nelec_and_multiplicity_even`, `nelec_and_multiplicity_odd`, and `multiplicity_is_possible`
    - Run nox for resorting imports

--- src/opi/input/structures/structure.py
+++ src/opi/input/structures/structure.py
@@ -170,6 +170,21 @@ class Structure:
         """Returns a boolean indicating if the multiplicity is even."""
         return self.multiplicity % 2 == 0
 
+    @property
+    def nelec_and_multiplicity_even(self) -> bool:
+        """Returns a boolean indicating if the number of electrons and the multiplicity are even."""
+        return self.nelec_is_even and self.multiplicity_is_even
+
+    @property
+    def nelec_and_multiplicity_odd(self) -> bool:
+        """Returns a boolean indicating if the number of electrons and the multiplicity are odd."""
+        return self.nelec_is_odd and self.multiplicity_is_odd
+
+    @property
+    def multiplicity_is_possible(self) -> bool:
+        """Returns a boolean indicating if the multiplicity can be realized with the number of electrons."""
+        return not (self.nelec_and_multiplicity_even or self.nelec_and_multiplicity_odd)
+
     def set_ls_multiplicity(self) -> None:
         """
         Sets `multiplicity` to the lowest possible multiplicity based on the number of electrons (`multiplicitiy`
@@ -211,16 +226,13 @@ class Structure:
                 String representation of Molecule
         """
         # > First we check whether the multiplicity is possible with the numbers of electrons and warn if not
-
-        # > nelec and multiplicity are even
-        if self.nelec_is_even and self.multiplicity_is_even:
+        if self.nelec_and_multiplicity_even:
             warn(
                 "Inconsistent input: an even number of electrons cannot have even multiplicity",
                 UserWarning,
             )
 
-        # > nelec and multiplicity are odd
-        if self.nelec_is_odd and self.multiplicity_is_odd:
+        if self.nelec_and_multiplicity_odd:
             warn(
                 "Inconsistent input: an odd number of electrons cannot have odd multiplicity",
                 UserWarning,
