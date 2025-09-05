972bd2069389e1f0a68098c0996cc06a7bbde8eb
Christoph Plett <plett@faccts.de>
2025-09-05 10:42:33 +0200

    Update Server/Client example
    Signed-off-by: Christoph Plett <plett@faccts.de>

--- examples/exmp046_server/job.py
+++ examples/exmp046_server/job.py
@@ -2,6 +2,8 @@
 
 from pathlib import Path
 import shutil
+import sys
+import warnings
 
 from opi.core import Calculator
 from opi.input.structures.structure import Structure
@@ -17,10 +19,11 @@ from opi.external_methods.server import OpiServer, ServerStatus
 # like logging into your HuggingFace account.
 # More information are given in the respective tutorial here:
 # https://www.faccts.de/docs/orca/6.1/tutorials/workflows/extopt.html
-#-------------------------------------------------------------------------
+# -------------------------------------------------------------------------
 
 # Initiate Server object
 server = OpiServer(
+    # Change the following path to a server script, e.g., from orca-external-tools
     serverpath="/path/to/working/server/script/from/orca-external-tools"
 )
 
@@ -28,12 +31,12 @@ server = OpiServer(
 status = server.start_server(exe="bash")
 if status != ServerStatus.RUNNING:
     if status == ServerStatus.ALREADY_RUNNING:
-        print("Some server was already running. Using old one.")
+        warnings.warn("Some server was already running. Using old one.")
     elif status == ServerStatus.PORT_IN_USE:
-        print("Port for server in use. Continue on your own risk.")
+        warnings.warn("Port for server in use. Continue on your own risk.")
     else:
         print(f"Server setup returned with error {status}")
-        exit(1)
+        sys.exit(1)
 
 # Working dir handling
 working_dir = Path("RUN")
@@ -42,10 +45,11 @@ working_dir.mkdir()
 
 # Calculator setup
 structure = Structure.from_xyz("h2o.xyz")
-orca_calc = Calculator(basename="uma_ext", working_dir=working_dir)
+orca_calc = Calculator(basename="extopt_ext", working_dir=working_dir)
 orca_calc.structure = structure
 orca_calc.input.ncores = 2
 orca_calc.input.add_simple_keywords(ExternalTools.EXTOPT, Opt.OPT)
+# Change the following path to a client script, e.g., from orca-external-tools
 path_client = "/path/to/working/client/script/from/orca-external-tools"
 orca_calc.input.add_blocks(BlockMethod(ProgExt=path_client))
 orca_calc.write_input()
